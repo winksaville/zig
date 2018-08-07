355435b4492a63ec4d476570fc84412205dec6cd
Wink Saville <wink@saville.com>
2018-08-07 13:09:46 -0700

    Disable test.atomicstore
    Add test.atomicrmw.xchg and test.atomicload

--- test/cases/atomic_store.zig
+++ test/cases/atomic_store.zig
@@ -2,14 +2,23 @@ const std = @import("std");
 const assert = std.debug.assert;
 const builtin = @import("builtin");
 const AtomicOrder = builtin.AtomicOrder;
+const AtomicRmwOp = builtin.AtomicRmwOp;
 
 
-test "atomicstore" {
-    var value: u8 = 123;
-    testAtomicStore(&value, 21);
-    assert(value == 21);
+test "test.atomicrmw.xchg" {
+    var data: u8 = 123;
+    var prev_value = @atomicRmw(u8, &data, AtomicRmwOp.Xchg, 42, AtomicOrder.SeqCst);
+    assert(prev_value == 123);
+    assert(data == 42);
 }
 
-fn testAtomicStore(ptr: *u8, value: u8) void {
-    @atomicStore(u8, ptr, value, AtomicOrder.Release);
+test "test.atomicload" {
+    var data: u8 = 123;
+    assert(@atomicLoad(u8, &data, AtomicOrder.Acquire) == 123);
 }
+
+//test "test.atomicstore" {
+//    var data: u8 = 123;
+//    @atomicStore(u8, &data, 5, AtomicOrder.Release);
+//    assert(data == 5);
+//}
